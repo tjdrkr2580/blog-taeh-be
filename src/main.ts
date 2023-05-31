3ebbdb7087356d2c44bc40e45f639062c09dd4a5
kimtaehyun <tjdrkr2580@naver.com>
2023-05-31 14:25:28 +0900

    fix : cors 에러

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'https://taeh-blog.kro.kr',
+  });
   await app.listen(4000);
 }
 bootstrap();
