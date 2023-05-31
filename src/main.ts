18b36048ed5119200bade13644d7c6bc51ff1f02
kimtaehyun <tjdrkr2580@naver.com>
2023-05-31 15:21:14 +0900

    fix .

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: ['https://taeh-blog.kro.kr', 'http://localhost:3000/'],
+    credentials: true,
   });
   await app.listen(4000);
 }
